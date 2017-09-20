e70b4bf1bef7c0987798b1b22459af6c325e56ba
Declan Vea <declanpvea@gmail.com>
2017-09-20 12:00:46 -0400

    final

--- src/components/getimageform.js
+++ src/components/getimageform.js
@@ -3,63 +3,59 @@ import '../styles/App.css';
 import GetImageButton from './getimagebutton';
 import ImageDisplay from './imagedisplay';
 
-const api_key = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=X3aNQZj7BdSiNk8EUjNBImIKplVeWrVEfeVislMQ';
+const api_key = 'nUDPdXQQtEn0kpWTbXv6rxXrfUeQyNzyGMDRmjPq';
 
 export default class GetImageForm extends React.Component {
   constructor(props){
     super(props);
 
     this.state = {
-      rover: [],
-      camera: "",
+      rover: "Curiosity",
+      camera: "FHAZ",
       images: [],
       sol: "",
     }
   }
 
-  handleRover = (e) => {
-    this.setState({rover: e.target.value});
-  }
-  handleCamera = (e) => {
-    this.setState({camera: e.target.value});
-  }
-  handleSol = (e) => {
-    e.preventDefault();
-    this.setState({sol: e.target.value});
-  }
+  handleRover = (e) => this.setState({rover: e.target.value});
+  handleCamera = (e) => this.setState({camera: e.target.value});
+  handleSol = (e) =>  this.setState({sol: e.target.value});
 
-  fetchRoverImage = () => {
-    console.log('clicked');
-    fetch(api_key)
+
+
+  fetchRoverImage = (e) => {
+    const {rover, camera, sol} = this.state;
+    let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=${api_key}`;
+
+    fetch(imageUrl)
     .then(r => r.json())
     .then(results => {
-    })
-  }
-  componentDidMount(){
-    this.fetchRoverImage();
+      this.setState({images: results.photos})
+    });
   }
 
 render(){
   console.log(this.state.rover);
+  const {rover, camera, sol} = this.state;
 return(
       <div>
         <label htmlFor="rover">Rover</label>
-          <select onChange={this.handleRover} id="rover" value={this.state.value}>
+          <select onChange={this.handleRover} id="rover" value={rover}>
             <option value="Curiosity">Curiosity</option>
             <option value="Opportunity">Opportunity</option>
             <option value="Spirit">Spirt</option>
           </select>
         <label htmlFor="camera">Camera Type</label>
-          <select onChange={this.handleCamera} id="rover" value={this.state.value}>
+          <select onChange={this.handleCamera} id="rover" value={camera}>
             <option value="fhaz">FHAZ (Front Hazard)</option>
             <option value="rhaz">RHAZ (Rear Hazard)</option>
             <option value="navcam">NAVCAM (Navigation Cam)</option>
           </select>
         <label htmlFor="sol">Martian Sol: 1000-2000</label>
-        <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.value}/>
+        <input type="number" onChange={this.handleSol} max="2000" min="1000" value={sol}/>
 
         <GetImageButton {...this.state} onClick={this.fetchRoverImage}/>
-        <ImageDisplay camera={this.state.camera} rover={this.state.rover} sol={this.state.sol}/>
+        <ImageDisplay {...this.state}/>
       </div>
       );
     }
--- src/components/imagedisplay.js
+++ src/components/imagedisplay.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 export default class ImageDisplay extends React.Component {
-render(){
-  return(
-    <div>{this.props.rover}</div>
-  )
-}
+  render(){
+    return(
+    <div>
+      <div>{this.props.rover}</div>
+        <ul>
+          {this.props.images.map((image, id) =>
+          <li key={image.id}><img src={image.img_src} alt="images"/></li>
+          )}
+        </ul>
+    </div>
+    )
+  }
 }
